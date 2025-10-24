636d3700104a9b70959dbb07a8476b817b1a252f
Myron Rodrigues <rodriguesmyron47@gmail.com>
2025-10-24 14:16:45 +0000

    reorder proprocessing

--- workspace/kenya_bird_classifier/bird_classifier/preprocessing.py
+++ workspace/kenya_bird_classifier/bird_classifier/preprocessing.py
@@ -1,11 +1,13 @@
 import librosa
+import noisereduce as nr
 import numpy as np
 
 
 def preprocess(signal, sample_r):
+  signal = nr.reduce_noise(y=signal, sr=sample_r, prop_decrease=0.9, stationary=True)
+  signal, _ = librosa.effects.trim(signal, top_db=30)
   signal = librosa.effects.preemphasis(signal)
   signal = librosa.util.normalize(signal)
-  signal, _ = librosa.effects.trim(signal, top_db=20)
   mel = librosa.feature.melspectrogram(y=signal, sr=sample_r, n_fft=2048, hop_length=512,
                                         n_mels=128, fmin=280, fmax=7600)
   mel_db = librosa.power_to_db(mel, ref=np.max)
